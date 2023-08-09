e0aaf0e95194c9378dc2612ac83d40d88e31416d
KhaDang2106 <kha.dangvunhat@gmail.com>
2023-08-09 15:05:33 +0700

    first commit

---
+++ btstate/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import BaiTapChonKinh from './components/BaiTapChonKinh/BaiTapChonKinh';
+
+function App() {
+  return (
+    <>
+      <BaiTapChonKinh />
+    </>
+  );
+}
+
+export default App;
---
+++ btstate/src/components/BaiTapChonKinh/BaiTapChonKinh.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class BaiTapChonKinh extends Component {
+  render() {
+    return <div>BaiTapChonKinh</div>;
+  }
+}
